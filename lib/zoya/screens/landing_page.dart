2dd7c77a660bc75d7a7890b1a5eb192ce8e79a3e
szoyaf <shaneyzoyaf@gmail.com>
2024-12-09 01:55:45 +0700

    feat: Get Community Forum

--- lib/zoya/screens/landing_page.dart
+++ lib/zoya/screens/landing_page.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:intl/intl.dart';
 import 'package:jajan_jogja_mobile/zoya/models/community_forum_entry.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 import '../../widgets/navbar.dart';
 
 void main() {
@@ -12,37 +16,49 @@ void main() {
 }
 
 class LandingPage extends StatelessWidget {
+  LandingPage({super.key});
+
   final List<String> carouselImages = [
     'https://images.unsplash.com/photo-1524985069026-dd778a71c7b4',
     'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4',
     'https://images.unsplash.com/photo-1482049016688-2d3e1b311543',
   ];
 
-  final List<CommunityForumEntry> communityEntries = [
-    CommunityForumEntry(
-      model: "community.forum",
-      pk: "1",
-      fields: Fields(
-        user: 1,
-        time: DateTime.now().subtract(const Duration(hours: 5)),
-        comment: "What's the best street food in Jogja?",
-      ),
-    ),
-    CommunityForumEntry(
-      model: "community.forum",
-      pk: "2",
-      fields: Fields(
-        user: 2,
-        time: DateTime.now().subtract(const Duration(hours: 12)),
-        comment: "Any recommendations for local coffee shops?",
-      ),
-    ),
-  ];
+  Future<String> fetchUsername(int userId, CookieRequest request) async {
+    final response = await request.get('http://127.0.0.1:8000/json-user/$userId/');
 
-  LandingPage({super.key});
+    // Check if the response is not null
+    if (response != null) {
+      // The response should already be a map (decoded JSON), so no need to jsonDecode it
+      var data = response;
+
+      // Extract the username from the response map
+      String username = data['username'] ?? 'Anonymous';
+
+      return username;
+    } else {
+      throw Exception('Failed to load username');
+    }
+  }
+
+  Future<List<CommunityForumEntry>> fetchCommunityForum(CookieRequest request) async {
+    final response = await request.get('http://127.0.0.1:8000/json-forum/');
+
+    var data = response;
+    
+    List<CommunityForumEntry> listCommunityForum = [];
+    for (var d in data) {
+      if (d != null) {
+        listCommunityForum.add(CommunityForumEntry.fromJson(d));
+      }
+    }
+    return listCommunityForum;
+  }
+
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Jajan Jogja'),
@@ -93,47 +109,84 @@ class LandingPage extends StatelessWidget {
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
-                  color: Color(0xFFE43D12),
+                  color: Color(0xFF7C1D05),
                 ),
               ),
               const SizedBox(height: 12),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: communityEntries.length,
-                itemBuilder: (context, index) {
-                  final entry = communityEntries[index];
-                  return Card(
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "User ${entry.fields.user}",
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF7C1D05),
-                            ),
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            DateFormat('yyyy-MM-dd HH:mm').format(entry.fields.time),
-                            style: const TextStyle(
-                              fontSize: 12,
-                              color: Color(0xFF7A7A7A),
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            entry.fields.comment,
-                            style: const TextStyle(fontSize: 16),
+
+              FutureBuilder<List<CommunityForumEntry>>(
+                future: fetchCommunityForum(request),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text('No forum data available'));
+                  } else {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF3F3F3),
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 6,
+                            offset: Offset(0, 2),
                           ),
                         ],
                       ),
-                    ),
-                  );
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: snapshot.data!.map((entry) {
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  FutureBuilder<String>(
+                                    future: fetchUsername(entry.fields.user, request),
+                                    builder: (context, userSnapshot) {
+                                      if (userSnapshot.connectionState == ConnectionState.waiting) {
+                                        return const CircularProgressIndicator();
+                                      } else if (userSnapshot.hasError) {
+                                        return Text('Error: ${userSnapshot.error}');
+                                      } else {
+                                        return Text(
+                                          "${userSnapshot.data}",
+                                          style: const TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                            color: Color(0xFFC98809),
+                                          ),
+                                        );
+                                      }
+                                    },
+                                  ),
+                                  const SizedBox(height: 6),
+                                  Text(
+                                    entry.fields.comment,
+                                    style: const TextStyle(fontSize: 14, color: Color(0xFF7A7A7A)),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  Text(
+                                    'Posted on ${DateFormat('yyyy-MM-dd').format(entry.fields.time)}',
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Color(0xFFC98809),
+                                    ),
+                                  ),
+                                  const Divider(color: Color(0xFFC98809)),
+                                ],
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    );
+                  }
                 },
               ),
             ],
